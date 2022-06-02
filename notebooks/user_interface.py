d6eab7f1902b88e598f441278ebd811b41090003
Christopher Peter <christopher.r.peter@gmail.com>
2022-06-02 18:51:29 +0200

    Mock-up frontend on Streamlit.

--- notebooks/user_interface.py
+++ notebooks/user_interface.py
@@ -4,17 +4,34 @@ import streamlit.components.v1 as html
 from  PIL import Image
 import numpy as np
 import pandas as pd
+import plotly.figure_factory as ff
 
-# 1. sidebar menu
+# 1. Menu
+# 1.1 Sidebar menu
+# with st.sidebar:
+#     selected = option_menu(
+#         menu_title = None,
+#         options=['Home', 'Visualisations', 'Contact'],
+#         icons=["house", "bar-chart-fill", "envelope"],
+#         menu_icon="cast",
+#         default_index=0,
+#         # orientation="horizontal",
+#         styles={
+#         "container": {"padding": "5!important", "background-color": "#fafafa"},
+#         "icon": {"color": "orange", "font-size": "25px"},
+#         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+#         "nav-link-selected": {"background-color": "#02ab21"},
+#     },
+#     )
 
-with st.sidebar:
-    selected = option_menu(
+# 1.2 Horizontal menu bar
+selected = option_menu(
         menu_title = None,
         options=['Home', 'Visualisations', 'Contact'],
         icons=["house", "bar-chart-fill", "envelope"],
         menu_icon="cast",
         default_index=0,
-        # orientation="horizontal",
+        orientation="horizontal",
         styles={
         "container": {"padding": "5!important", "background-color": "#fafafa"},
         "icon": {"color": "orange", "font-size": "25px"},
@@ -23,6 +40,7 @@ with st.sidebar:
     },
     )
 
+
 if selected == "Home":
     # st.title(f"You have selected {selected} directory.")
     st.markdown('''
@@ -30,11 +48,28 @@ if selected == "Home":
             **BatchLegal** will give you an overview! _Cool_?
             ''')
     input_keywords = st.text_input("", placeholder = "Please type in your keyword...")
-    st.write(f'**_{input_keywords}_**')
+    st.write(f'**{input_keywords}**')
 
+# random visualisations
 if selected == "Visualisations":
     st.title(f"You have selected {selected}")
     # st.bar_chart()
+    # Add histogram data
+    x1 = np.random.randn(200) - 2
+    x2 = np.random.randn(200)
+    x3 = np.random.randn(200) + 2
+
+    # Group data together
+    hist_data = [x1, x2, x3]
+
+    group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+    # Create distplot with custom bin_size
+    fig = ff.create_distplot(
+            hist_data, group_labels, bin_size=[.1, .25, .5])
+
+    # Plot!
+    st.plotly_chart(fig, use_container_width=True)
 
 if selected == "Contact":
     st.title(f"You have selected {selected}")
